b55bd3a4d9c2f821350a4d984bdf7d1e383d52c0
Hsiao, Cooper <cooper.hsiao@commscope.com>
2024-04-25 18:45:20 +0800

    add postadservice for receiving kafka ad.data.

---
+++ src/postadservice/src/main/java/adservice/postadservice/PostadserviceApplication.java
@@ -0,0 +1,13 @@
+package adservice.postadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostadserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostadserviceApplication.class, args);
+	}
+
+}
---
+++ src/postadservice/src/test/java/adservice/postadservice/PostadserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package adservice.postadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostadserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
