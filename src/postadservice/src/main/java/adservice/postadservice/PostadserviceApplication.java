0478a5a8dbd798cbe79978d131c090105b69b4db
Hsiao, Cooper <cooper.hsiao@commscope.com>
2024-04-29 17:57:09 +0800

    elastic search okay

--- src/postadservice/src/main/java/adservice/postadservice/PostadserviceApplication.java
+++ src/postadservice/src/main/java/adservice/postadservice/PostadserviceApplication.java
@@ -1,12 +1,16 @@
 package adservice.postadservice;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.UpdateRequest;
+import co.elastic.clients.transport.DefaultTransportOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.concurrent.TimeUnit;
+import java.util.Map;
+import java.util.UUID;
 import java.util.function.Consumer;
 
 @SpringBootApplication
@@ -18,12 +22,27 @@ public class PostadserviceApplication {
 	}
 
 	@Bean
-	public Consumer<String> readAdData() {
+	public Consumer<String> readAdData(
+			ElasticsearchClient es
+	) {
 		return (data) -> {
 			LOG.info("Read ad data: {}", data);
             try {
-                TimeUnit.MILLISECONDS.sleep(100);
-            } catch (InterruptedException e) {
+				UpdateRequest<Map, Map> req = new UpdateRequest.Builder<Map, Map>()
+						.index("postadservice")
+						.id(UUID.randomUUID().toString())
+						.docAsUpsert(true)
+						.doc(Map.of(
+								"ad", data
+						))
+						.build();
+				es.withTransportOptions(
+								new DefaultTransportOptions.Builder()
+										.setHeader("Content-Type", "application/json")
+										.build()
+						)
+						.update(req, Map.class);
+			} catch (Exception e) {
 				LOG.error("ERROR!!!", e);
             }
         };
