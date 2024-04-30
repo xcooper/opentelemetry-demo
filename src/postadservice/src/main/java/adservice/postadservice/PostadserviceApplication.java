41c92b145fc14a2d75a35394a8d908e0ca51fba8
Hsiao, Cooper <cooper.hsiao@commscope.com>
2024-04-30 18:29:19 +0800

    add jdbc

--- src/postadservice/src/main/java/adservice/postadservice/PostadserviceApplication.java
+++ src/postadservice/src/main/java/adservice/postadservice/PostadserviceApplication.java
@@ -3,12 +3,16 @@ package adservice.postadservice;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch.core.UpdateRequest;
 import co.elastic.clients.transport.DefaultTransportOptions;
+import com.zaxxer.hikari.HikariDataSource;
+import org.h2.Driver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
 import java.util.Map;
 import java.util.UUID;
 import java.util.function.Consumer;
@@ -22,12 +26,24 @@ public class PostadserviceApplication {
 	}
 
 	@Bean
+	public DataSource dataSource() {
+		var ds = new HikariDataSource();
+		ds.setJdbcUrl("jdbc:h2:mem:test");
+		ds.setDriverClassName(Driver.class.getName());
+		ds.setUsername("sa");
+		return ds;
+	}
+
+	@Bean
 	public Consumer<String> readAdData(
-			ElasticsearchClient es
+			ElasticsearchClient es,
+			JdbcTemplate jt
 	) {
+		jt.execute("CREATE TABLE addata(data VARCHAR)");
 		return (data) -> {
-			LOG.info("Read ad data: {}", data);
             try {
+				jt.update("INSERT INTO addata VALUES ( ? )", data);
+				LOG.info("Inserted the AD data {} to DB", data);
 				UpdateRequest<Map, Map> req = new UpdateRequest.Builder<Map, Map>()
 						.index("postadservice")
 						.id(UUID.randomUUID().toString())
@@ -42,6 +58,7 @@ public class PostadserviceApplication {
 										.build()
 						)
 						.update(req, Map.class);
+				LOG.info("Index the AD data {} to ES", data);
 			} catch (Exception e) {
 				LOG.error("ERROR!!!", e);
             }
